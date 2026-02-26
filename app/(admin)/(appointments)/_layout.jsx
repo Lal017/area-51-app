fe2461da296ef54d04ae45c538961fceca8f5a87
Eduardo Salazar <eduardos842@gmail.com>
2026-02-25 17:38:23 -0800

    fix(admin): remove extra stack screen from (appointments) layout file.

--- app/(admin)/(appointments)/_layout.jsx
+++ app/(admin)/(appointments)/_layout.jsx
@@ -6,7 +6,6 @@ const AppointmentsLayout = () =>
     return (
         <Stack>
             <Stack.Screen name="index" options={{title: 'Appointments Console', header: () => <CustHeader title='Appointments'/>}}/>
-            <Stack.Screen name="appointmentList" options={{title: 'Appointment List', header: () => <CustHeader title='Appointments'/>}}/>
             <Stack.Screen name="appointmentView" options={{title: 'Appointment', header: () => <CustHeader title='Appointment'/>}}/>
         </Stack>
     );
